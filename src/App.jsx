1dac4ee45cc296bcc233096a9a842bc2cdede84b
PranjalMewaraa <mewarapranjal089@gmail.com>
2025-03-24 17:12:29 +0530

    `Added react-icons package and updated import in App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import OnBoarding from "./Pages/Onboarding/OnBoarding";
+import Wall from "./Layout/Wall";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <OnBoarding />
+      <Wall />
     </>
   );
 }
---
+++ src/Layout/Wall.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventCard from "../components/EventCard";
+
+const Wall = () => {
+  return (
+    <div className="p-4">
+      <EventCard />
+    </div>
+  );
+};
+
+export default Wall;
