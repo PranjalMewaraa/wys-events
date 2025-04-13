117d12b5804535ad6ded677a24a2a2fcafd02eca
PranjalMewaraa <mewarapranjal089@gmail.com>
2025-04-14 01:27:09 +0530

    ss

--- src/components/Chat/MessageRenderer.jsx
+++ src/components/Chat/MessageRenderer.jsx
@@ -4,10 +4,10 @@ import useEventDetails from "../../utils/hooks/event";
 import ReviewPopUp from "./ReviewPopup";
 import ChatReview from "./ChatReview";
 
-const MessageRenderer = ({ message,eventId }) => {
+const MessageRenderer = ({ message, eventId }) => {
   const { type, content } = message;
-  const [isPopupOpen,setIsPopupOpen]=useState(false);
-  const {userRole}=useEventDetails(eventId);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const { userRole } = useEventDetails(eventId);
   switch (type) {
     case "text":
       return <p>{content}</p>;
@@ -15,40 +15,42 @@ const MessageRenderer = ({ message,eventId }) => {
     case "rsvp":
       return (
         <div className="space-y-1">
-          <p className=" abeezee-regular text-[14px] text-[#FFFFFF]">{content.question}</p>
+          <p
+            className={` abeezee-regular text-[14px] ${
+              userRole === "host" ? "text-black" : "text-gray-400"
+            } `}
+          >
+            {content.question}
+          </p>
           {content.attendees && (
             <div className="flex -space-x-2">
               {content.attendees.map((a, i) => (
-               <img
-               key={i}
-               src={a.avatar}
-               className="w-6 h-6 rounded-full border-2 border-white"
-               title={a.name}
-             />
-                
+                <img
+                  key={i}
+                  src={a.avatar}
+                  className="w-6 h-6 rounded-full border-2 border-white"
+                  title={a.name}
+                />
               ))}
             </div>
           )}
-          {userRole==="seeker" && (
-            <button className="mt-1 text-xs border border-[#F38E1C]  text-[#F38E1C] rounded-3xl  px-3 py-2  bg-transparent"
-
-            onClick={()=>{
-              setIsPopupOpen(true)
-            }}
-             >
+          {userRole === "seeker" && (
+            <button
+              className="mt-1 text-xs border border-[#F38E1C]  text-[#F38E1C] rounded-3xl  px-3 py-2  bg-transparent"
+              onClick={() => {
+                setIsPopupOpen(true);
+              }}
+            >
               {content.buttonText}
             </button>
-            
-          )
-          }
-          {isPopupOpen &&(
+          )}
+          {isPopupOpen && (
             <ChatPopup
-           isOpen={isPopupOpen}
-           onClose={()=>setIsPopupOpen(false)}
-           eventId={eventId}
-           isPopupOpen={isPopupOpen}
-           />
-           
+              isOpen={isPopupOpen}
+              onClose={() => setIsPopupOpen(false)}
+              eventId={eventId}
+              isPopupOpen={isPopupOpen}
+            />
           )}
         </div>
       );
@@ -56,27 +58,36 @@ const MessageRenderer = ({ message,eventId }) => {
     case "review":
       return (
         <div className="space-y-1">
-          <p className="abeezee-regular text-[14px] text-white">{content.question}</p>
-          {content.buttonVisible && (
-            userRole ==="seeker"?<button className="mt-1 text-xs border border-[#F38E1C]  text-[#F38E1C] rounded-3xl  px-3 py-2  bg-transparent" onClick={()=>{
-              setIsPopupOpen(true)
-            }} >
-              {content.buttonText}
-            </button>:
-            <button className="mt-1 text-xs border border-white  text-white rounded-3xl px-3 py-2 bg-transparent"  onClick={()=>{
-              setIsPopupOpen(true)
-            }}>
-            {content.buttonText}
-          </button>
-          )}
-           {isPopupOpen &&(
+          <p className="abeezee-regular text-[14px] text-white">
+            {content.question}
+          </p>
+          {content.buttonVisible &&
+            (userRole === "seeker" ? (
+              <button
+                className="mt-1 text-xs border border-[#F38E1C]  text-[#F38E1C] rounded-3xl  px-3 py-2  bg-transparent"
+                onClick={() => {
+                  setIsPopupOpen(true);
+                }}
+              >
+                {content.buttonText}
+              </button>
+            ) : (
+              <button
+                className="mt-1 text-xs border border-white  text-white rounded-3xl px-3 py-2 bg-transparent"
+                onClick={() => {
+                  setIsPopupOpen(true);
+                }}
+              >
+                {content.buttonText}
+              </button>
+            ))}
+          {isPopupOpen && (
             <ChatReview
-           isOpen={isPopupOpen}
-           onClose={()=>setIsPopupOpen(false)}
-           eventId={eventId}
-           isPopupOpen={isPopupOpen}
-           />
-           
+              isOpen={isPopupOpen}
+              onClose={() => setIsPopupOpen(false)}
+              eventId={eventId}
+              isPopupOpen={isPopupOpen}
+            />
           )}
         </div>
       );
